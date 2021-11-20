6f47228339005452bb219c714c12f0f067eecbd9
Darok2 <90783586+Darok2@users.noreply.github.com>
2021-11-20 00:02:24 -0500

    create repositorios

---
+++ BackEndRentaCar/src/repositories/index.ts
@@ -0,0 +1,7 @@
+export * from './administrador.repository';
+export * from './asesor.repository';
+export * from './cliente.repository';
+export * from './empresa.repository';
+export * from './persona.repository';
+export * from './solicitud.repository';
+export * from './vehiculo.repository';
